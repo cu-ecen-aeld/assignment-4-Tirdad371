a8d9da43f82cacbe7cccaf8b07b6f1da859461e6
Tirdad <muhammadnajafi4201@gmil.com>
2026-04-06 13:29:15 +0200

    Update mk file to use finder-app subdirectory

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -3,17 +3,17 @@ AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Tird
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 $(@D)/writer              $(TARGET_DIR)/usr/bin/writer
-    $(INSTALL) -m 0755 $(@D)/finder.sh           $(TARGET_DIR)/usr/bin/finder.sh
-    $(INSTALL) -m 0755 $(@D)/finder-test.sh      $(TARGET_DIR)/usr/bin/finder-test.sh
-    $(INSTALL) -m 0755 $(@D)/tester.sh           $(TARGET_DIR)/usr/bin/tester.sh
-    $(INSTALL) -d                                $(TARGET_DIR)/etc/finder-app/conf
-    $(INSTALL) -m 0644 $(@D)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/conf/
-    $(INSTALL) -m 0644 $(@D)/conf/username.txt   $(TARGET_DIR)/etc/finder-app/conf/
+    $(INSTALL) -m 0755 $(@D)/finder-app/writer              $(TARGET_DIR)/usr/bin/writer
+    $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh           $(TARGET_DIR)/usr/bin/finder.sh
+    $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh      $(TARGET_DIR)/usr/bin/finder-test.sh
+    $(INSTALL) -m 0755 $(@D)/finder-app/tester.sh           $(TARGET_DIR)/usr/bin/tester.sh
+    $(INSTALL) -d                                           $(TARGET_DIR)/etc/finder-app/conf
+    $(INSTALL) -m 0644 $(@D)/conf/assignment.txt            $(TARGET_DIR)/etc/finder-app/conf/
+    $(INSTALL) -m 0644 $(@D)/conf/username.txt              $(TARGET_DIR)/etc/finder-app/conf/
 endef
 
 $(eval $(generic-package))
